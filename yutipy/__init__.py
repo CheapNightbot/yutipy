19299e54ca51d0b9512cc12d6114bcbad2fe230d
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-03-28 12:18:44 +0900

    Update __init__.py

--- yutipy/__init__.py
+++ yutipy/__init__.py
@@ -1,19 +1,17 @@
 from .deezer import Deezer
 from .itunes import Itunes
 from .kkbox import KKBox
-from .models import MusicInfo
 from .musicyt import MusicYT
 from .spotify import Spotify
 from .yutipy_music import YutipyMusic
+from . import exceptions
 
 __all__ = [
     "Deezer",
     "Itunes",
     "KKBox",
-    "MusicInfo",
     "MusicYT",
     "Spotify",
     "YutipyMusic",
+    "exceptions"
 ]
-
-__version__ = "1.4.22"
