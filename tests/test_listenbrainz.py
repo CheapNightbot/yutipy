ce9846ad803f6a916be5735360cd4a43fe6972bf
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-07 03:34:11 +0900

    Add tests for ListenBrainz activity method

--- tests/test_listenbrainz.py
+++ tests/test_listenbrainz.py
@@ -28,10 +28,45 @@ class MockResponseUsers(BaseResponse):
         }
 
 
+class MockResponseActivity(BaseResponse):
+    @staticmethod
+    def json():
+        return {
+            "payload": {
+                "count": 1,
+                "listens": [
+                    {
+                        "playing_now": True,
+                        "track_metadata": {
+                            "additional_info": {
+                                "duration": 269,
+                                "media_player": "Some Music Player",
+                                "origin_url": "https://music.youtube.com/watch?v=lYBUbBu4W08",
+                                "submission_client": "Some Scrobbler Plugin",
+                            },
+                            "artist_name": "Artist",
+                            "track_name": "Song",
+                        },
+                    }
+                ],
+                "playing_now": True,
+                "user_id": "Username",
+            }
+        }
+
+
 @pytest.fixture
 def mock_response_users(listenbrainz, monkeypatch):
     def mock_get(*args, **kwargs):
         return MockResponseUsers()
+
+    monkeypatch.setattr(listenbrainz._ListenBrainz__session, "get", mock_get)
+
+
+@pytest.fixture
+def mock_response_activity(listenbrainz, monkeypatch):
+    def mock_get(*args, **kwargs):
+        return MockResponseActivity()
 
     monkeypatch.setattr(listenbrainz._ListenBrainz__session, "get", mock_get)
 
@@ -52,3 +87,17 @@ def test_find_user_case_insensitive(listenbrainz, mock_response_users):
     user_name = listenbrainz.find_user(username=username)
     assert user_name is not None
     assert username.lower() == user_name.lower()
+
+
+def test_get_currently_playing(listenbrainz, mock_response_activity):
+    username = "Username"
+    activity = listenbrainz.get_currently_playing(username)
+    assert activity.is_playing is True
+    assert activity.title
+    assert activity.artists
+    assert activity.url
+
+def test_get_currently_playing_no_user(listenbrainz, mock_response_activity):
+    username = "Bob"
+    activity = listenbrainz.get_currently_playing(username)
+    assert activity is None
